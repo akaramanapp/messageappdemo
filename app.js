fbfab44eed9dbe9eabf866a64441af2ac09076ce
akaraman <akaraman@albarakaturk.com.tr>
2020-12-12 13:49:07 +0300

    add variable

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
+const Pusher = require("pusher");
 const PORT = process.env.PORT || 5000
- 
-var cities = [{name: 'Istanbul', country: 'Turkey'}, {name: 'New York', country: 'USA'}, {name: 'London', country:'England'}];
+
+const pusher = new Pusher({
+    appId: process.env.appId,
+    key: process.env.key,
+    secret: process.env.secret,
+    cluster: "eu",
+    useTLS: true
+});
 
 var app = express();
 
-app.get('/api/cities', function(request, response){
-   response.send(cities);
+app.get('/api/event', function (request, response) {
+    pusher.trigger("my-channel", "my-event", {
+        message: "Event Message"
+    });
 });
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
